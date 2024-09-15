d730317c673ffdbf9d6b9a60b25969ffa2ff973f
Sujal-Gupta-SG <Sujal24092004+github@gmail.com>
2024-09-16 01:28:08 +0530

    update

--- app/layout.js
+++ app/layout.js
@@ -18,6 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <SessionProvider>
       <html lang="en">
+        <title>Stock Manager</title>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
