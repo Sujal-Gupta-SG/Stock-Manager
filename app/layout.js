6a50c2ffeb21d05970cad8fe7de428b1e7a419cb
Sujal-Gupta-SG <Sujal24092004+github@gmail.com>
2024-09-16 01:51:37 +0530

    update

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@
 import { SessionProvider } from "next-auth/react";
 import localFont from "next/font/local";
 import "./globals.css";
+import Head from "next/head";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -18,7 +19,14 @@ export default function RootLayout({ children }) {
   return (
     <SessionProvider>
       <html lang="en">
-        <title>Stock Manager</title>
+        <Head>
+          <link
+            rel="icon"
+            type="image/svg+xml"
+            href="data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' fill='none' stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' class='w-10 h-10 text-white p-2 bg-indigo-500 rounded-full' viewBox='0 0 24 24'><path d='M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5'></path></svg>"
+          />
+          <title>Stock Manager</title>
+        </Head>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
